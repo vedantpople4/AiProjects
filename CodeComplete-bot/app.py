6a13d87395d5d0dab29392ee9035bb39155efd55
Vedant Pople <vedantpople4@gmail.com>
2025-02-01 10:53:48 -0700

    Bot changes

--- CodeComplete-bot/app.py
+++ CodeComplete-bot/app.py
@@ -53,7 +53,7 @@ with st.sidebar:
     st.header("⚙️ Configuration")
     selected_model = st.selectbox(
         "Choose Model",
-        ["deepseek-r1:1.5b", "deepseek-r1:3b"],
+        ["Deepseek-r1:1.5b", "Deepseek-r1:3b"],
         index=0
     )
     st.divider()
